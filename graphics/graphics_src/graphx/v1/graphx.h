10f7c777f1c95fc22c5115af32f8e3509045c9d1
Matt Waltz <MateoConLechuga@users.noreply.github.com>
2016-06-01 13:01:23 -0700

    Fixed some naming
    This is already in the release; just thought I would fix it here

--- graphics/graphics_src/graphx/v1/graphx.h
+++ graphics/graphics_src/graphx/v1/graphx.h
@@ -293,8 +293,8 @@ void gfx_SwapDraw(void);
 void gfx_Blit(uint8_t buffer);
 void gfx_BlitLines(uint8_t buffer, uint8_t y_loc, uint8_t num_lines);
 void gfx_BlitArea(uint8_t buffer, uint24_t x, uint8_t y, uint24_t width, uint24_t height);
-#define gfx_BlitBuffer() gfx_Blit(gfx_buffer)
-#define gfx_BlitScreen() gfx_Blit(gfx_screen)
+#define gfx_BlitBuffer() gfx_Blit(gfx_screen)
+#define gfx_BlitScreen() gfx_Blit(gfx_buffer)
 
 /**
  * Outputs a character at the current cursor position
